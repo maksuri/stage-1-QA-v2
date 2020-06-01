4dfe46932e42aa8a4238b2586d844239275e97cc
Maxim Lugovtsov <promaxhd@yandex.ru>
2020-06-01 18:41:12 +0300

    remove cleanCode

--- src/main/java/cleanCode/planes/PostPlane.java
+++
@@ -1,7 +0,0 @@
-package planes;
-
-public class PostPlane extends Plane {
-    public PostPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance,maxLoadCapacity);
-    }
-}
